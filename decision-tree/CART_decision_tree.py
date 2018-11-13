e20c850d657264acc3162081960287fc9d412d0a
JonZhao <1044264932@qq.com>
2018-11-13 21:00:03 +0800

    minor updation

--- decision-tree/CART_decision_tree.py
+++ decision-tree/CART_decision_tree.py
@@ -2,7 +2,7 @@ import operator
 import treePlotter
 from base import is_all_sample_same, get_most_common_class, split_data_set_by_operate, \
     calculate_gini, get_validation_error_count_by_major_class, get_validation_error_count_by_tree, get_keys_for_dict, \
-    get_validation_error_before_pruning, get_validation_error_after_pruning
+    get_validation_error_before_pruning, get_validation_error_after_pruning, create_data_set_two
 
 """
 周志华《机器学习》第四章中的习题4.4 code
@@ -10,59 +10,18 @@ from base import is_all_sample_same, get_most_common_class, split_data_set_by_op
 """
 
 
-def create_data_set():
-    """
-    色泽-》 0：青绿 | 1：乌黑 | 2：浅白
-    根蒂-》 0：蜷缩 | 1：稍缩 | 2：硬挺
-    敲声-》 0：浊响 | 1：沉闷 | 2：清脆
-    纹理-》 0：清晰 | 1：稍糊 | 2：模糊
-    脐部-》 0：凹陷 | 1：稍凹 | 2：平坦
-    触感-》 0：硬滑 | 1：软粘
-    密度-》 是一个连续值
-    含糖量-》 是一个连续值
-
-    结果-》 好瓜：Y | 坏瓜：N
-    """
-    data_set_train = [[0, 0, 0, 0, 0, 0, '好瓜'],
-                      [1, 0, 1, 0, 0, 0, '好瓜'],
-                      [1, 0, 0, 0, 0, 0, '好瓜'],
-                      [0, 0, 1, 0, 0, 0, '好瓜'],
-                      [0, 1, 0, 0, 1, 1, '好瓜'],
-                      [1, 1, 0, 1, 1, 1, '好瓜'],
-                      [0, 2, 2, 0, 2, 1, '坏瓜'],
-                      [2, 1, 1, 1, 0, 0, '坏瓜'],
-                      [1, 1, 0, 0, 1, 1, '坏瓜'],
-                      [2, 0, 0, 2, 2, 0, '坏瓜'],
-                      [0, 0, 1, 1, 1, 0, '坏瓜']]
-    data_set_validate = [[2, 0, 0, 0, 0, 0, '好瓜'],
-                         [1, 1, 0, 0, 1, 0, '好瓜'],
-                         [1, 1, 1, 1, 1, 0, '坏瓜'],
-                         [2, 2, 2, 2, 2, 0, '坏瓜'],
-                         [2, 0, 0, 2, 2, 1, '坏瓜'],
-                         [0, 1, 0, 1, 0, 0, '坏瓜']]
-    labels_list = ['色泽', '根蒂', '敲声', '纹理', '脐部', '触感']
-    labels_dict = {'色泽': {0: '青绿', 1: '乌黑', 2: '浅白'},
-                   '根蒂': {0: '蜷缩', 1: '稍缩', 2: '硬挺'},
-                   '敲声': {0: '浊响', 1: '沉闷', 2: '清脆'},
-                   '纹理': {0: '清晰', 1: '稍糊', 2: '模糊'},
-                   '脐部': {0: '凹陷', 1: '稍凹', 2: '平坦'},
-                   '触感': {0: '硬滑', 1: '软粘'}}
-    is_features_discrete = [1, 1, 1, 1, 1, 1, 0, 0]
-    return data_set_train, data_set_validate, labels_list, labels_dict, is_features_discrete
-
-
-def get_best_feature_gini(data_set, labels_list, is_features_discrete):
+def get_best_feature_gini(data_set, features_list, is_features_discrete):
     best_gini_index = 100
     best_gini_index_index = -1
     continuous_feature_value = None
-    for i in range(len(labels_list)):
+    for i in range(len(features_list)):
         # 处理离散型特征
-        sub_features_list = [sample[i] for sample in data_set]
-        unique_sub_features = set(sub_features_list)
+        feature_value_list = [sample[i] for sample in data_set]
+        feature_value_list = set(feature_value_list)
         if is_features_discrete[i] == 1:
             gini_index = 0.0
             # 在一个feature上根据feature的取值的不同，划分成不同的sub data set
-            for value in unique_sub_features:
+            for value in feature_value_list:
                 sub_data_set = split_data_set_by_operate(data_set, i, value, operator.eq, delete_col=False)
                 prob = float(len(sub_data_set))/len(data_set)
                 gini_index += prob * calculate_gini(sub_data_set)
@@ -73,11 +32,11 @@ def get_best_feature_gini(data_set, labels_list, is_features_discrete):
         else:
             continuous_best_gini_index = 100
             continuous_best_feature_value = -1
-            unique_sub_features = sorted(unique_sub_features, reverse=False)
+            feature_value_list = sorted(feature_value_list, reverse=False)
             new_unique_sub_features = []
             # 对于连续型特征，将其都有的取值进行排序，取连续两个取值的中值作为待测试划分
-            for j in range(len(unique_sub_features) - 1):
-                new_unique_sub_features.append((unique_sub_features[j] + unique_sub_features[j + 1]) / 2)
+            for j in range(len(feature_value_list) - 1):
+                new_unique_sub_features.append((feature_value_list[j] + feature_value_list[j + 1]) / 2)
             # 对于所有计算得到的中值，进行测试
             for value in new_unique_sub_features:
                 # 一个中值可以将数据分为两部分
@@ -95,12 +54,12 @@ def get_best_feature_gini(data_set, labels_list, is_features_discrete):
     return best_gini_index_index, continuous_feature_value
 
 
-def tree_generate_without_pruning(data_set_train, labels_list, labels_dict, is_features_discrete):
+def tree_generate_without_pruning(data_set_train, features_list, features_dict, is_features_discrete):
     """
     树生成 不剪枝
     :param data_set_train:
-    :param labels_list:
-    :param labels_dict:
+    :param features_list:
+    :param features_dict:
     :param is_features_discrete:
     :return:
     """
@@ -109,54 +68,54 @@ def tree_generate_without_pruning(data_set_train, labels_list, labels_dict, is_f
     if class_list.count(class_list[0]) == len(class_list):
         return class_list[0]
 
-    # 检查labels是否为空, dataSet在labels上的取值都一样（所有样本在所有属性上的取值一样）
-    if len(labels_list) == 0 or is_all_sample_same(data_set_train):
+    # 检查features_list是否为空, dataSet在feature上的取值都一样（所有样本在所有属性上的取值一样）
+    if len(features_list) == 0 or is_all_sample_same(data_set_train):
         return get_most_common_class(data_set_train)
 
     # 从A中找出最优的属性值，进行划分
-    best_feature_index, best_continuous_feature_value = get_best_feature_gini(data_set_train, labels_list, is_features_discrete)
-    best_feature_label = labels_list[best_feature_index]
+    best_feature_index, best_continuous_feature_value = get_best_feature_gini(data_set_train, features_list, is_features_discrete)
+    best_feature_name = features_list[best_feature_index]
 
     if is_features_discrete[best_feature_index] == 1:
         # 如果该特征是离散型的，从数据中删除该特征
-        del (labels_list[best_feature_index])
-        tree = {best_feature_label: {}}
-        sub_features_value_set = labels_dict[best_feature_label].keys()
+        del (features_list[best_feature_index])
+        tree = {best_feature_name: {}}
+        feature_value_set = features_dict[best_feature_name].keys()
 
-        for sub_feature_value in sub_features_value_set:
-            sub_labels_list = labels_list[:]
-            sub_data_set_train = split_data_set_by_operate(data_set_train, best_feature_index, sub_feature_value,
+        for feature_value in feature_value_set:
+            new_features_list = features_list[:]
+            sub_data_set_train = split_data_set_by_operate(data_set_train, best_feature_index, feature_value,
                                                            operator.eq, delete_col=True)
-            sub_feature_name = labels_dict[best_feature_label][sub_feature_value]
+            sub_feature_name = features_dict[best_feature_name][feature_value]
             # 如果划分出来的子属性集合为空，则将分支结点标记为叶节点，其分类标记为data_set中样本最多的类
             if len(sub_data_set_train) == 0:
-                tree[best_feature_label][sub_feature_name] = get_most_common_class(data_set_train)
+                tree[best_feature_name][sub_feature_name] = get_most_common_class(data_set_train)
             # 如果划分出来的子属性集合不为空，则继续递归
             else:
-                tree[best_feature_label][sub_feature_name] = tree_generate_without_pruning(sub_data_set_train,
-                                                                                           sub_labels_list, labels_dict,
+                tree[best_feature_name][sub_feature_name] = tree_generate_without_pruning(sub_data_set_train,
+                                                                                          new_features_list, features_dict,
                                                                                            is_features_discrete)
     else:
         # 如果该特征是连续的，不需要从数据中删除该特征
         # 与离散属性不同，若当前结点划分属性为连续属性，该属性还可作为其后代结点的划分属性
-        key = best_feature_label + '<=' + str.format("%0.3f" % best_continuous_feature_value)
+        key = best_feature_name + '<=' + str.format("%0.3f" % best_continuous_feature_value)
         tree = {key: {}}
         sub_data_set_le = split_data_set_by_operate(
             data_set_train, best_feature_index, best_continuous_feature_value, operator.le, delete_col=False)
         sub_data_set_gt = split_data_set_by_operate(
             data_set_train, best_feature_index, best_continuous_feature_value, operator.gt, delete_col=False)
-        tree[key]['是'] = tree_generate_without_pruning(sub_data_set_le, labels_list, labels_dict, is_features_discrete)
-        tree[key]['否'] = tree_generate_without_pruning(sub_data_set_gt, labels_list, labels_dict, is_features_discrete)
+        tree[key]['是'] = tree_generate_without_pruning(sub_data_set_le, features_list, features_dict, is_features_discrete)
+        tree[key]['否'] = tree_generate_without_pruning(sub_data_set_gt, features_list, features_dict, is_features_discrete)
     return tree
 
 
-def tree_generate_with_pre_pruning(data_set_train, data_set_validate, labels_list, labels_dict, is_features_discrete):
+def tree_generate_with_pre_pruning(data_set_train, data_set_validate, features_list, features_dict, is_features_discrete):
     """
     预剪枝 树生成
     :param data_set_train:
     :param data_set_validate:
-    :param labels_list:
-    :param labels_dict:
+    :param features_list:
+    :param features_dict:
     :param is_features_discrete:
     :return:
     """
@@ -165,17 +124,17 @@ def tree_generate_with_pre_pruning(data_set_train, data_set_validate, labels_lis
     if class_list.count(class_list[0]) == len(class_list):
         return class_list[0]
 
-    # 检查labels是否为空, dataSet在labels上的取值都一样（所有样本在所有属性上的取值一样）
-    if len(labels_list) == 0 or is_all_sample_same(data_set_train):
+    # 检查features_list是否为空, dataSet在feature上的取值都一样（所有样本在所有属性上的取值一样）
+    if len(features_list) == 0 or is_all_sample_same(data_set_train):
         return get_most_common_class(data_set_train)
 
     # 从A中找出最优的属性值，进行划分
-    best_feature_index, best_continuous_feature_value = get_best_feature_gini(data_set_train, labels_list, is_features_discrete)
-    best_feature_label = labels_list[best_feature_index]
+    best_feature_index, best_continuous_feature_value = get_best_feature_gini(data_set_train, features_list, is_features_discrete)
+    best_feature_name = features_list[best_feature_index]
 
     # 特征值为离散值
     if is_features_discrete[best_feature_index] == 1:
-        del (labels_list[best_feature_index])
+        del (features_list[best_feature_index])
     accuracy_rate_before_pruning = get_validation_error_before_pruning(data_set_train, data_set_validate)
     accuracy_rate_after_pruning = get_validation_error_after_pruning(data_set_train, data_set_validate, best_feature_index,
                                                                      is_features_discrete[best_feature_index], best_continuous_feature_value)
@@ -186,21 +145,21 @@ def tree_generate_with_pre_pruning(data_set_train, data_set_validate, labels_lis
     # 划分之后的准确率超过了划分之前的准确率，继续进行划分
     else:
         if is_features_discrete[best_feature_index] == 1:
-            tree = {best_feature_label: {}}
+            tree = {best_feature_name: {}}
             sub_features_value_set = [sample[best_feature_index] for sample in data_set_train]
             sub_features_value_set = set(sub_features_value_set)
 
             for sub_feature_value in sub_features_value_set:
-                sub_labels_list = labels_list[:]
+                new_features_list = features_list[:]
                 sub_data_set_train = split_data_set_by_operate(data_set_train, best_feature_index, sub_feature_value,
                                                                operator.eq, delete_col=True)
                 sub_data_set_validate = split_data_set_by_operate(data_set_validate, best_feature_index, sub_feature_value,
                                                                   operator.eq, delete_col=True)
-                sub_feature_name = labels_dict[best_feature_label][sub_feature_value]
-                tree[best_feature_label][sub_feature_name] = \
-                    tree_generate_with_pre_pruning(sub_data_set_train, sub_data_set_validate, sub_labels_list, labels_dict, is_features_discrete)
+                sub_feature_name = features_dict[best_feature_name][sub_feature_value]
+                tree[best_feature_name][sub_feature_name] = \
+                    tree_generate_with_pre_pruning(sub_data_set_train, sub_data_set_validate, new_features_list, features_dict, is_features_discrete)
         else:
-            key = best_feature_label + '<=' + str.format("%0.3f" % best_continuous_feature_value)
+            key = best_feature_name + '<=' + str.format("%0.3f" % best_continuous_feature_value)
             tree = {key: {}}
             # 划分数据集
             sub_data_set_train_le = split_data_set_by_operate(
@@ -212,43 +171,43 @@ def tree_generate_with_pre_pruning(data_set_train, data_set_validate, labels_lis
             sub_data_set_validate_gt = split_data_set_by_operate(
                 data_set_validate, best_feature_index, best_continuous_feature_value, operator.gt, delete_col=False)
             # 生成节点
-            tree[key]['是'] = tree_generate_with_pre_pruning(sub_data_set_train_le, sub_data_set_validate_le, labels_list, labels_dict, is_features_discrete)
-            tree[key]['否'] = tree_generate_with_pre_pruning(sub_data_set_train_gt, sub_data_set_validate_gt, labels_list, labels_dict, is_features_discrete)
+            tree[key]['是'] = tree_generate_with_pre_pruning(sub_data_set_train_le, sub_data_set_validate_le, features_list, features_dict, is_features_discrete)
+            tree[key]['否'] = tree_generate_with_pre_pruning(sub_data_set_train_gt, sub_data_set_validate_gt, features_list, features_dict, is_features_discrete)
         return tree
 
 
-def tree_generator_with_post_pruning(input_tree, data_set_train, data_set_validate, labels_list, labels_dict):
+def tree_generator_with_post_pruning(input_tree, data_set_train, data_set_validate, features_list, features_dict):
     """
     后剪枝 树生成
     :param input_tree:
     :param data_set_train:
     :param data_set_validate:
-    :param labels_list:
-    :param labels_dict:
+    :param features_list:
+    :param features_dict:
     :return:
     """
     if type(input_tree).__name__ == 'str':
         return input_tree
     feature_name = list(input_tree.keys())[0]
     inferior_tree = input_tree[feature_name]
-    feature_index = labels_list.index(feature_name)
+    feature_index = features_list.index(feature_name)
 
     for value in inferior_tree:
-        value_key = get_keys_for_dict(labels_dict[feature_name], value)[0]
+        value_key = get_keys_for_dict(features_dict[feature_name], value)[0]
 
-        sub_labels_list = labels_list[:]
-        del(sub_labels_list[feature_index])
+        new_features_list = features_list[:]
+        del(new_features_list[feature_index])
 
         sub_data_set_train = split_data_set_by_operate(data_set_train, feature_index, value_key,
                                                        operator.eq, delete_col=True)
         sub_data_set_validate = split_data_set_by_operate(data_set_validate, feature_index, value_key,
                                                           operator.eq, delete_col=True)
         input_tree[feature_name][value] = tree_generator_with_post_pruning(
-            inferior_tree[value], sub_data_set_train, sub_data_set_validate, sub_labels_list, labels_dict)
+            inferior_tree[value], sub_data_set_train, sub_data_set_validate, new_features_list, features_dict)
     # 到达某个结点之后，测试一下准确率，看看是否要剪枝
     # get_validation_error_count_by_major_class(major_class, data_set_validate)
-    # get_validation_error_count_by_tree(input_tree, data_set_validate, labels_list)
-    error_count_by_tree = get_validation_error_count_by_tree(input_tree, data_set_validate, labels_list, labels_dict)
+    # get_validation_error_count_by_tree(input_tree, data_set_validate, features_list)
+    error_count_by_tree = get_validation_error_count_by_tree(input_tree, data_set_validate, features_list, features_dict)
     error_count_by_major_class = get_validation_error_count_by_major_class(get_most_common_class(data_set_train), data_set_validate)
     if error_count_by_tree <= error_count_by_major_class:
         return input_tree
@@ -256,8 +215,8 @@ def tree_generator_with_post_pruning(input_tree, data_set_train, data_set_valida
 
 
 if __name__ == '__main__':
-    data_set_train, data_set_validate, labels_list, labels_dict, is_features_discrete = create_data_set()
-    tree_without_pruning = tree_generate_without_pruning(data_set_train, labels_list[:], labels_dict, is_features_discrete)
-    tree_with_pre_pruning = tree_generate_with_pre_pruning(data_set_train, data_set_validate, labels_list[:], labels_dict, is_features_discrete)
-    tree_with_post_pruning = tree_generator_with_post_pruning(tree_without_pruning, data_set_train, data_set_validate, labels_list[:], labels_dict)
+    data_set_train, data_set_validate, features_list, features_dict, is_features_discrete = create_data_set_two()
+    tree_without_pruning = tree_generate_without_pruning(data_set_train, features_list[:], features_dict, is_features_discrete)
+    tree_with_pre_pruning = tree_generate_with_pre_pruning(data_set_train, data_set_validate, features_list[:], features_dict, is_features_discrete)
+    tree_with_post_pruning = tree_generator_with_post_pruning(tree_without_pruning, data_set_train, data_set_validate, features_list[:], features_dict)
     treePlotter.createPlot(tree_with_post_pruning)
--- decision-tree/ID3_&C4.5_algorithm.py
+++ decision-tree/ID3_&C4.5_algorithm.py
@@ -1,7 +1,8 @@
 from math import log
 import operator
 import treePlotter
-from base import is_all_sample_same, get_most_common_class, split_data_set_by_operate, calculate_information_entropy
+from base import is_all_sample_same, get_most_common_class, split_data_set_by_operate, calculate_information_entropy, \
+    create_data_set_one
 
 """
 周志华《机器学习》第四章中的习题4.3 code
@@ -10,104 +11,43 @@ ID3基于信息增益，C4.5基于增益率
 """
 
 
-def create_data_set():
-    """
-    色泽-》 0：青绿 | 1：乌黑 | 2：浅白
-    根蒂-》 0：蜷缩 | 1：稍缩 | 2：硬挺
-    敲声-》 0：浊响 | 1：沉闷 | 2：清脆
-    纹理-》 0：清晰 | 1：稍糊 | 2：模糊
-    脐部-》 0：凹陷 | 1：稍凹 | 2：平坦
-    触感-》 0：硬滑 | 1：软粘
-    密度-》 是一个连续值
-    含糖量-》 是一个连续值
-
-    结果-》 好瓜：Y | 坏瓜：N
-
-    data_set = [['青绿', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', 0.697, 0.460, '好瓜'],
-                ['乌黑', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', 0.774, 0.376, '好瓜'],
-                ['乌黑', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', 0.634, 0.264, '好瓜'],
-                ['青绿', '蜷缩', '沉闷', '清晰', '凹陷', '硬滑', 0.608, 0.318, '好瓜'],
-                ['浅白', '蜷缩', '浊响', '清晰', '凹陷', '硬滑', 0.556, 0.215, '好瓜'],
-                ['青绿', '稍缩', '浊响', '清晰', '稍凹', '软粘', 0.403, 0.237, '好瓜'],
-                ['乌黑', '稍缩', '浊响', '稍糊', '稍凹', '软粘', 0.481, 0.149, '好瓜'],
-                ['乌黑', '稍缩', '浊响', '清晰', '稍凹', '硬滑', 0.437, 0.211, '好瓜'],
-                ['乌黑', '稍缩', '沉闷', '稍糊', '稍凹', '硬滑', 0.666, 0.091, '坏瓜'],
-                ['青绿', '硬挺', '清脆', '清晰', '平坦', '软粘', 0.243, 0.267, '坏瓜'],
-                ['浅白', '硬挺', '清脆', '模糊', '平坦', '硬滑', 0.245, 0.057, '坏瓜'],
-                ['浅白', '蜷缩', '浊响', '模糊', '平坦', '软粘', 0.343, 0.099, '坏瓜'],
-                ['青绿', '稍缩', '浊响', '稍糊', '凹陷', '硬滑', 0.639, 0.161, '坏瓜'],
-                ['浅白', '稍缩', '沉闷', '稍糊', '凹陷', '硬滑', 0.657, 0.198, '坏瓜'],
-                ['乌黑', '稍缩', '浊响', '清晰', '稍凹', '软粘', 0.360, 0.370, '坏瓜'],
-                ['浅白', '蜷缩', '浊响', '模糊', '平坦', '硬滑', 0.593, 0.042, '坏瓜'],
-                ['青绿', '蜷缩', '沉闷', '稍糊', '稍凹', '硬滑', 0.719, 0.103, '坏瓜']]
-    """
-    data_set = [[0, 0, 0, 0, 0, 0, 0.697, 0.460, '好瓜'],
-                [1, 0, 1, 0, 0, 0, 0.774, 0.376, '好瓜'],
-                [1, 0, 0, 0, 0, 0, 0.634, 0.264, '好瓜'],
-                [0, 0, 1, 0, 0, 0, 0.608, 0.318, '好瓜'],
-                [2, 0, 0, 0, 0, 0, 0.556, 0.215, '好瓜'],
-                [0, 1, 0, 0, 1, 1, 0.403, 0.237, '好瓜'],
-                [1, 1, 0, 1, 1, 1, 0.481, 0.149, '好瓜'],
-                [1, 1, 0, 0, 1, 0, 0.437, 0.211, '好瓜'],
-                [1, 1, 1, 1, 1, 0, 0.666, 0.091, '坏瓜'],
-                [0, 2, 2, 0, 2, 1, 0.243, 0.267, '坏瓜'],
-                [2, 2, 2, 2, 2, 0, 0.245, 0.057, '坏瓜'],
-                [2, 0, 0, 2, 2, 1, 0.343, 0.099, '坏瓜'],
-                [0, 1, 0, 1, 0, 0, 0.639, 0.161, '坏瓜'],
-                [2, 1, 1, 1, 0, 0, 0.657, 0.198, '坏瓜'],
-                [1, 1, 0, 0, 1, 1, 0.360, 0.370, '坏瓜'],
-                [2, 0, 0, 2, 2, 0, 0.593, 0.042, '坏瓜'],
-                [0, 0, 1, 1, 1, 0, 0.719, 0.103, '坏瓜']]
-    labels_list = ['色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '密度', '含糖量']
-    labels_dict = {'色泽': {0: '青绿', 1: '乌黑', 2: '浅白'},
-                   '根蒂': {0: '蜷缩', 1: '稍缩', 2: '硬挺'},
-                   '敲声': {0: '浊响', 1: '沉闷', 2: '清脆'},
-                   '纹理': {0: '清晰', 1: '稍糊', 2: '模糊'},
-                   '脐部': {0: '凹陷', 1: '稍凹', 2: '平坦'},
-                   '触感': {0: '硬滑', 1: '软粘'},
-                   '密度': {},
-                   '含糖量': {}}
-    is_features_discrete = [1, 1, 1, 1, 1, 1, 0, 0]
-    return data_set, labels_list, labels_dict, is_features_discrete
-
-
-def tree_generate(data_set, labels_list, labels_dict, is_features_discrete, ID3_or_C45):
+def tree_generate(data_set, features_list, features_dict, is_features_discrete, ID3_or_C45):
     # 检查样本是否已经同属于一类了
     class_list = [sample[-1] for sample in data_set]
     if class_list.count(class_list[0]) == len(class_list):
         return class_list[0]
 
-    # 检查labels是否为空, dataSet在labels上的取值都一样（所有样本在所有属性上的取值一样）
-    if len(labels_list) == 0 or is_all_sample_same(data_set):
+    # 检查features是否为空, dataSet在features上的取值都一样（所有样本在所有属性上的取值一样）
+    if len(features_list) == 0 or is_all_sample_same(data_set):
         return get_most_common_class(data_set)
 
     # 从A中找出最优的属性值，进行划分
-    best_feature_index, best_continuous_feature_value = get_best_feature(data_set, labels_list, is_features_discrete, ID3_or_C45)
-    best_feature_label = labels_list[best_feature_index]
+    best_feature_index, best_continuous_feature_value = get_best_feature(data_set, features_list, is_features_discrete, ID3_or_C45)
+    best_feature_label = features_list[best_feature_index]
     # 如果该特征是离散型的，从数据中删除该特征
     if is_features_discrete[best_feature_index] == 1:
         tree = {best_feature_label: {}}
 
         del(is_features_discrete[best_feature_index])
-        del (labels_list[best_feature_index])
+        del (features_list[best_feature_index])
 
-        unique_sub_features = labels_dict[best_feature_label].keys()
+        unique_sub_features = features_dict[best_feature_label].keys()
         for value in unique_sub_features:
             # 往下继续生成树
-            # 使用sub_labels来替代labels，传递到tree_generate函数，tree_generate会删除labels中的内容，
-            # 如果一直传递同一个labels，会出现问题，sub_labels = labels[:]，相当于拷贝一个新的labels list
-            sub_labels_list = labels_list[:]
+            # 使用new_features_list来替代features_list，传递到tree_generate函数，tree_generate会删除features_list中的内容，
+            # 如果一直传递同一个features_list，会出现问题，new_features_list = features_list[:]，相当于拷贝一个新的features list
+            new_features_list = features_list[:]
             sub_is_feature_discrete = is_features_discrete[:]
             sub_data_set = split_data_set_by_operate(
                 data_set, best_feature_index, value, operator.eq, delete_col=True)
-            sub_feature_name = labels_dict[best_feature_label][value]
+            sub_feature_name = features_dict[best_feature_label][value]
             # 如果划分出来的子属性集合为空，则将分支结点标记为叶节点，其分类标记为data_set中样本最多的类
             if len(sub_data_set) == 0:
                 tree[best_feature_label][sub_feature_name] = get_most_common_class(data_set)
             # 如果划分出来的子属性集合不为空，则继续递归
             else:
-                tree[best_feature_label][sub_feature_name] = tree_generate(sub_data_set,
-                    sub_labels_list, labels_dict, sub_is_feature_discrete, ID3_or_C45)
+                tree[best_feature_label][sub_feature_name] = \
+                    tree_generate(sub_data_set, new_features_list, features_dict, sub_is_feature_discrete, ID3_or_C45)
     # 如果该特征是连续的，不需要从数据中删除该特征
     # 与离散属性不同，若当前结点划分属性为连续属性，该属性还可作为其后代结点的划分属性
     else:
@@ -115,21 +55,21 @@ def tree_generate(data_set, labels_list, labels_dict, is_features_discrete, ID3_
         tree = {key: {}}
         tree[key]['是'] = tree_generate(split_data_set_by_operate(
             data_set, best_feature_index, best_continuous_feature_value, operator.le, delete_col=False),
-            labels_list, labels_dict, is_features_discrete, ID3_or_C45)
+            features_list, features_dict, is_features_discrete, ID3_or_C45)
         tree[key]['否'] = tree_generate(split_data_set_by_operate(
             data_set, best_feature_index, best_continuous_feature_value, operator.gt, delete_col=False),
-            labels_list, labels_dict, is_features_discrete, ID3_or_C45)
+            features_list, features_dict, is_features_discrete, ID3_or_C45)
     return tree
 
 
-def get_best_feature(data_set, labels, is_features_discrete, ID3_or_C45):
+def get_best_feature(data_set, features, is_features_discrete, ID3_or_C45):
     """
     从lables中找出最优划分属性
     :param data_set:
-    :param labels:
+    :param features:
     :return:
     """
-    feature_num = len(labels)
+    feature_num = len(features)
     base_entropy = calculate_information_entropy(data_set)
     gain_list = []
     gain_ratio_list = []
@@ -203,6 +143,6 @@ def get_best_feature(data_set, labels, is_features_discrete, ID3_or_C45):
 
 if __name__ == '__main__':
     ID3_or_C45 = 0
-    data_set, labels_list, labels_dict, is_features_discrete = create_data_set()
-    decision_tree = tree_generate(data_set, labels_list, labels_dict, is_features_discrete, ID3_or_C45)
+    data_set, features_list, features_dict, is_features_discrete = create_data_set_one()
+    decision_tree = tree_generate(data_set, features_list, features_dict, is_features_discrete, ID3_or_C45)
     treePlotter.createPlot(decision_tree)
